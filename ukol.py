f0aef2d9caac36d8b695c0660eeae4d760bdebf7
Cubeik007 <79248529+Cubeik007@users.noreply.github.com>
2023-11-08 10:29:53 +0100

    Update ukol.py

--- ukol.py
+++ ukol.py
@@ -1,5 +1,6 @@
 from typing import Optional
-from math import sqrt
+from math import sqrt, cos, pi
+
 
 def common_elements(f_list: list[int], 
                     s_list: list[int]) -> list[int]:
@@ -19,6 +20,7 @@ def common_elements(f_list: list[int],
             pos2 += 1
     return list(set(common))
 
+
 def fibonaci(n: int) -> int:
     """returns n-th fibonatchi number"""
     if type(n) != int or n < 1:
@@ -28,6 +30,7 @@ def fibonaci(n: int) -> int:
     elif n == 2:
         return 1
     return fibonaci(n-1)+fibonaci(n-2)
+
 
 def quadratic(a: float, b = 0.0, c = 0.0) -> list[Optional[float]]:
     """returns solution of quadratic equations with coefficients a, b c"""
@@ -39,16 +42,90 @@ def quadratic(a: float, b = 0.0, c = 0.0) -> list[Optional[float]]:
     else:
         dif = sqrt(det)
         return [(b+dif)/2*a, (b-dif)/2*a]
-        
-    
-def histogram(my_list = []):
-    my_dict = {}
-    my_list 
-
-
-print(quadratic(1, 4, 4))
-# print(common_elements(sorted([8, 7, 12, 1, 5, 12]), sorted([3, 12, 1, 4, 6, 9, 11])))
-
-
-        
          
+
+def histogram(my_str: str) -> dict:
+    my_dict = {}
+    my_set = set([i for i in my_str  if i.isalpha()])
+
+
+def square_root(n: int) -> Optional[int]:
+    """Returns a square root of number.
+
+    If number is not integer or is negative, raises error.
+    """
+    if (type(n) is not int or n < 0):
+        raise Exception("Your input is invalid, you can use integers only!")
+    l = 0
+    r = n
+
+    while l <= r:
+        mid = (l+r)//2
+        guess = mid**2
+        if guess == n:
+            return mid
+        elif guess < n:
+            l = mid+1
+        else: 
+            r = mid-1
+    return None
+
+
+def x_cos_x() -> Optional[int]:
+    """Returns a square root of number.
+
+    If number is not integer or is negative, raises error.
+    """
+    
+    l = 0
+    r = 2*pi
+
+    while l <= r:
+        mid = (l+r)/2
+        guess = round(mid-cos(mid), 4)
+        if guess == 0:
+            return mid
+        elif guess < 0:
+            l = mid
+        else: 
+            r = mid
+    return None
+
+
+def bubble_sort(x: list[int]) -> list[int]:
+    """Returns ordered list using bubble sort."""
+
+    my_len = len(x)-1
+    i = 0
+    for j in range(my_len):
+        for i in range(my_len):
+            if x[i]>x[i+1]:
+                x[i], x[i+1] = x[i+1], x[i]
+    return x
+
+
+def sorting_words(x: list[str], key=0) -> list[str]:
+    """Returns lexicolographicaly sorted list."""
+
+    my_len = len(x)-1
+    i = 0
+    for j in range(my_len):
+        for i in range(my_len): 
+            x[i], x[i+1] = x[i+1], x[i]
+            if x[i][key]>x[i+1][key]:
+                x[i], x[i+1] = x[i+1], x[i]
+    check_list = []
+    l = 0
+    for i in range(my_len):
+        if x[i][key] == x[i+1][key]:
+            l += 1
+        else:
+            check_list.append(l)
+    new_list = []
+    key += 1
+    for index in range(len(check_list)-1):
+        new_list.append(sorting_words(x[check_list[index]:check_list[index+1]], key))
+    return x
+
+
+print(sorting_words(["jablko", "jahoda", "bagr", "bricho", "svedsko"]))
